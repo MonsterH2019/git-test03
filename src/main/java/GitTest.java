ba1989273f67ac950f9be8ef8d9f99fcbe8cbc71
FayClu <FayClu.com>
2023-09-03 18:31:05 +0800

    first commit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,14 @@
+/**
+ * ClassName: GitTest
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @Author FayClu
+ * @Create 2023/9/3 18:30
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
